dbdfad8816b82bdc0a150940142b5f909cfc53c1
redantcode2021 <redantcyber@gmail.com>
2023-01-15 22:24:16 +0700

    Root project of Supabase app

---
+++ foo_supabase/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.foo_supabase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ foo_supabase/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:foo_supabase/app/app.dart';
+import 'package:foo_supabase/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
