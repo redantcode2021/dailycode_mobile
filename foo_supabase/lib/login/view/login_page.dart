5ecf564597f530a42c3384bc68d47f84bcdd0112
redantcode2021 <redantcyber@gmail.com>
2023-03-26 14:30:25 +0700

    Refactoring some utilities

---
+++ foo_supabase/lib/login/view/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foo_supabase/login/login.dart';
+import 'package:user_repository/user_repository.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  static Page<void> page() => const MaterialPage<void>(child: LoginPage());
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => LoginBloc(context.read<UserRepository>()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sign In'),
+        ),
+        body: const LoginView(),
+      ),
+    );
+  }
+}
---
+++ foo_supabase/lib/login/view/login_view.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foo_supabase/gen/assets.gen.dart';
+import 'package:foo_supabase/login/bloc/login_bloc.dart';
+import 'package:formz/formz.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.symmetric(
+        vertical: 50,
+        horizontal: 32,
+      ),
+      children: const [
+        _Header(),
+        SizedBox(height: 18),
+        _EmailInput(),
+        SizedBox(height: 28),
+        _SendEmailButton(),
+        SizedBox(height: 28),
+        if (!kIsWeb) OpenEmailButton()
+      ],
+    );
+  }
+}
+
+class OpenEmailButton {}
+
+class _SendEmailButton extends StatelessWidget {
+  const _SendEmailButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<LoginBloc>().state;
+
+    return ElevatedButton(
+      key: const Key('loginView_sendEmail_button'),
+      onPressed: state.status.isInProgress || !state.valid
+          ? null
+          : () => context.read<LoginBloc>().add(
+                LoginSubmitted(
+                  email: state.email.value,
+                  isWeb: kIsWeb,
+                ),
+              ),
+      child: Text(state.status.isInProgress ? 'Loading' : 'Send Magic Link'),
+    );
+  }
+}
+
+class _EmailInput extends StatelessWidget {
+  const _EmailInput();
+
+  @override
+  Widget build(BuildContext context) {
+    final isInProgress = context.select(
+      (LoginBloc bloc) => bloc.state.status == FormzSubmissionStatus.inProgress,
+    );
+
+    return TextFormField(
+      key: const Key('loginView_emailInput_textField'),
+      readOnly: isInProgress,
+      onChanged: (email) {
+        context.read<LoginBloc>().add(LoginEmailChanged(email));
+      },
+      decoration: const InputDecoration(labelText: 'Email'),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      key: const Key('loginView_header'),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox.square(
+              dimension: 100,
+              child: Assets.images.supabase.image(),
+            ),
+            SizedBox(
+              width: 200,
+              height: 100,
+              child: Assets.images.vgv.image(),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Text(
+            'Sign in via the magic Link',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        ),
+      ],
+    );
+  }
+}
